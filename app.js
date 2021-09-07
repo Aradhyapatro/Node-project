6f9941ec9e10bbf7751a50213e74a696888ad54b
aradhya <aradhyapatro14@gmail.com>
2021-09-07 18:25:48 +0530

    reinitialize

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Started Node");
+//jshint esversion:6
+const buzz = () => {
+    console.log('buzz');
+};
+
+const fizz = () => {
+    console.log('fuzz');
+};
+
+const fiz = () => {
+    console.log("12345");
+};
+
+const buzzfizz = () => {
+    console.log('buzzfizz');
+    setTimeout(fiz, 1000);
+    buzz();
+    fizz();
+};
+
+buzzfizz();
+
+let promise = new Promise(function(resolve, reject) {
+    setTimeout(() => reject(new Error("Oops")), 1000);
+});
+
+promise.then(
+    results => {
+        console.log(results);
+    }, error => {
+        console.log(error);
+    }
+);
